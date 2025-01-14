177501437bbdd9b5cb6e1304d10e9f897b6d0958
Xinyu Mu <mxy2233@outlook.com>
2025-01-14 17:11:35 +0800

    微调

--- gm/configs.py
+++ gm/configs.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass, asdict
 class TrainingConfig:
     num_epochs: int = 10
     kl_weight: float = 1e-6
-    gan_weight: float = 0.0
-    gan_loss_start: int = 0
+    gan_weight: float = 0.01
+    gan_loss_start: int = 2
     num_visualization: int = 1
     lr: float = 3e-4
     grad_clip: float = 0.0
